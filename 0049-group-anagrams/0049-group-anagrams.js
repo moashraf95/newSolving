dbfa8352f935e7ab9d167189de273f736a217209
Mohamed Ashraf Ibrahim <109323494+moashraf95@users.noreply.github.com>
2024-01-13 23:26:37 +0200

    Time: 96 ms (91.26%), Space: 53.6 MB (48.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,53 +1,27 @@
 var groupAnagrams = function (strs) {
 
     let map1 = new Map();
-    map1.set(0, [strs[0]])
-    let tracker = 0;
+    let map2 = new Map();
+    let tracker = 0
+    for (let i = 0 ; i< strs.length ; i++){
+        const sortedWord = [...strs[i]].sort().join('') ;
 
-    for (let i = 1; i < strs.length; i++) {
-
-        for (let j = 0; j <= tracker; j++) {
-            const comparedValue = map1.get(j);
-            const areAnagrams = compareAnagrams(strs[i], comparedValue[0]);
-
-            if (areAnagrams) {
-                const newComparedValue = [...comparedValue, strs[i]];
-                map1.set(j, newComparedValue);
-                 break;
-            } else if (j == tracker && !areAnagrams) {
-                tracker ++ ;
-                map1.set(tracker, [strs[i]]);
-                break;
-            }
+        if(!map1.has(sortedWord)){
+            map1.set(sortedWord,[strs[i]]);
+            map2.set(tracker,sortedWord);
+            tracker ++ ;
+        }else{
+            const pastValue = map1.get(sortedWord);
+            const newValue = [...pastValue , strs[i]];
+            map1.set(sortedWord,newValue);
         }
-
     }
 
     const result = [];
 
     for (let i = 0; i < map1.size; i++) {
-        result.push(map1.get(i))
+        result.push(map1.get(map2.get(i)))
     }
 
     return result;
-
-
 };
-
-
-// O(n)
-const compareAnagrams = function (s, t) {
-
-    if (s.length != t.length) return false;
-    const arr = new Array(26).fill(0);
-    for (let i = 0; i < s.length; i++) {
-        arr[s[i].charCodeAt() - 'a'.charCodeAt()]++;
-        arr[t[i].charCodeAt() - 'a'.charCodeAt()]--;
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] != 0) return false;
-    }
-
-    return true;
-}
